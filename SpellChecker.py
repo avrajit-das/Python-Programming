3844c71ba3a0d7802a7ce84ecb5701665d3d570b
SUBID DAS <itsmesubid@gmail.com>
2022-12-17 21:56:14 +0530

    Fix `typoError`

--- SpellChecker.py
+++ SpellChecker.py
@@ -1,14 +1,14 @@
-from textblob import Textlob
-
+from textblob import TextBlob
 def Convert(string):
-	li = list(string.split())
-	return li
-str1 = input("Enter your text: ")
+    li = list(string.split())
+    return li
+str1= input("Enter your text: ") 
+print("Example: I am good boy or girl")
 words=Convert(str1)
 corrected_words = []
 for i in words:
-	corrected_words.append(TextBlob(i))
-print("Wrong spellings :", words)
-print("Corrected spellings are :")
+    corrected_words.append(TextBlob(i))
+print("Worng spelling: ", words)
+print("Corrected spelling are: ")
 for i in corrected_words:
-	print(i.correct(), end= " ")
+    print(i.correct(), end=" ")
